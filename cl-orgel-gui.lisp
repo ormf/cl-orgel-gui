698bf4ad48caa9cbf035ffe9b27cba27cf6d08e5
ormf <junk14@selma.hfmdk-frankfurt.de>
2023-07-24 20:19:56 +0200

    created widget-defs and fixed numbox ui behaviour
    made multi-vsliders autoadjustable.

--- cl-orgel-gui.lisp
+++ cl-orgel-gui.lisp
@@ -19,10 +19,14 @@
 ;;; **********************************************************************
 
 (in-package :cl-orgel-gui)
-(setf (orgel-numbox *curr-orgel-state*) "0")
+
 (defstruct orgel
-  (numbox 0.0)
+  (numbox "0.0")
   (level-sliders (make-array 16)))
+
+(defparameter *curr-orgel-state*
+  (make-orgel :level-sliders (make-array 16 :initial-element "0.0")
+              :numbox "0"))
 
 (defparameter *background-color* "#607d8b")
 
@@ -33,9 +37,6 @@
 (defparameter *vsl-colors*
   (map 'vector (lambda (idx)  (aref *colors* idx)) '(0 0 1 0 2 1 3 0 4 2 5 1 6 3 7 0)))
 
-(defparameter *curr-orgel-state*
-  (make-orgel :level-sliders (make-array 16 :initial-element 0.0)))
-
 (defvar *global-connection-hash*
   (make-hash-table* :test 'equalp)
   "map connection to orgel form elems.")
@@ -44,24 +45,25 @@
   (format nil "~a" (uuid:make-v1-uuid)))
 
 (defun synchronize-vsl (idx val self)
-  (setf (aref (orgel-level-sliders *curr-orgel-state*) idx) val)
-  (maphash (lambda (connection-id connection-hash)
-             (declare (ignore connection-id))
-             (let ((orgel (gethash "orgel" connection-hash)))
-               (when orgel (let ((elem (aref (orgel-level-sliders orgel)
-                                             idx)))
-                             (unless (equal self elem) (setf (value elem) val))))))
-           clog-connection::*connection-data*))
+  (let ((val-string (ensure-string val)))
+    (setf (aref (orgel-level-sliders *curr-orgel-state*) idx) val-string)
+    (maphash (lambda (connection-id connection-hash)
+               (declare (ignore connection-id))
+               (let ((orgel (gethash "orgel" connection-hash)))
+                 (when orgel (let ((elem (aref (orgel-level-sliders orgel) idx)))
+                               (unless (equal self elem) (setf (value elem) val-string))))))
+             clog-connection::*connection-data*)))
 
 (defun synchronize-numbox (val self)
-  (setf (orgel-numbox *curr-orgel-state*) val)
-  (maphash (lambda (connection-id connection-hash)
-             (declare (ignore connection-id))
-             (let ((orgel (gethash "orgel" connection-hash)))
-               (when orgel
-                 (let ((elem (orgel-numbox (gethash "orgel" connection-hash))))
-                   (unless (equal self elem) (setf (value elem) val))))))
-           clog-connection::*connection-data*))
+  (let ((val-string (ensure-string val)))
+    (setf (orgel-numbox *curr-orgel-state*) val-string)
+    (maphash (lambda (connection-id connection-hash)
+               (declare (ignore connection-id))
+               (let ((orgel (gethash "orgel" connection-hash)))
+                 (when orgel
+                   (let ((elem (orgel-numbox (gethash "orgel" connection-hash))))
+                     (unless (equal self elem) (setf (value elem) val-string))))))
+             clog-connection::*connection-data*)))
 
 
 
@@ -97,6 +99,8 @@
                                     (declare (ignore obj))()))))
 
 
+(defparameter *vsliders* nil)
+
 (defun on-new-window (body)
   (let ((orgel (make-orgel))
         connection-id)
@@ -111,7 +115,7 @@
     (setf (gethash "orgel" (gethash connection-id clog-connection::*connection-data*))
           orgel)
     (with-connection-cache (body)
-      (let* (p1 nbs1 nb1 ms1 vsliders)
+      (let* (p1 nbs1 nb1 vsliders)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
         ;; Panel 1 contents
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -125,51 +129,28 @@
         ;; Create form for panel 1
 ;;;        (create-br p1)
         (setf nbs1 (create-div p1)) ;;; container for numberbox(es)
-        (setf nb1 (numbox nbs1 :label "freq"))
-        (setf ms1  (create-div p1 :style "color: transparent; border: none;width: 100px;height: 100px;display: flex;")) ;;; container der multisliders
-        (setf vsliders (v-collect (n 16) (vslider
-                                          ms1
-                                          :background-color *background-color*
-                                          :color (aref *vsl-colors* n)
-                                          :thumbcolor "transparent")))
+        (setf nb1 (numbox nbs1 :label "freq" :color "black" :background-color "#fff"))
+;;;        (setf ms1  (create-div p1 :style "color: transparent; border: none;width: 100px;height: 100px;display: flex;")) ;;; container der multisliders
+        ;; (setf vsliders (v-collect (n 16) (vslider
+        ;;                                   ms1
+        ;;                                   :background-color *background-color*
+        ;;                                   :color (aref *vsl-colors* n)
+        ;;                                   :thumbcolor "transparent")))
+        (setf vsliders
+              (multi-vslider p1 :num 16 :width 160 :colors *vsl-colors* :background-color "transparent"
+                                         :receiver-fn #'synchronize-vsl))
+
         (create-br p1)
         (multi-vslider p1 :colors *vsl-colors* :background-color "transparent")
-
-        (setf (value nb1) (orgel-numbox *curr-orgel-state*))
-        (setf (orgel-numbox orgel) nb1)
         (loop for vsl in vsliders
               for idx from 0
               do (progn
-                   (setf (value vsl) (aref (orgel-level-sliders *curr-orgel-state*) idx))
-;;;                   (setf (width vsl) 100)
-;;;                   (setf (height vsl) 10)
-                   (let ((idx idx) (vsl vsl)) ;;; close around function to catch the current value for idx and vsl
-                     (setf (aref (orgel-level-sliders orgel) idx) vsl)
-                     (set-on-input vsl
-                                   (lambda (obj)
-                                     (declare (ignore obj))
-                                     (let ((val (value vsl)))
-;;;                                       (format t "vsl~a: ~a~%" (1+ idx) val)
-                                       (synchronize-vsl idx val vsl))))
-                     ;; (set-on-mouse-down vsl
-                     ;;                    (lambda (obj event-data)
-                     ;;                      (declare (ignore obj event-data))
-                     ;;                      (setf *mouse-down* t)))
-                     ;; (set-on-mouse-up vsl
-                     ;;                    (lambda (obj event-data)
-                     ;;                      (declare (ignore obj event-data))x
-                     ;;                      (setf *mouse-down* nil)))
-                     (set-on-mouse-move
-                      vsl
-                      (lambda (obj event-data)
-                        (declare (ignore obj))
-                        (when (getf event-data :shift-key)
-                          (let ((value (- 100 (getf event-data :y))))
-;;;                                              (format t "vsl~a: ~a~%" (1+ idx) event-data)
-                            (when value
-                              (setf (value vsl) value)
-                              (synchronize-vsl idx value vsl)
-                              ))))))))
+                   (setf (value vsl)
+                         (aref (orgel-level-sliders *curr-orgel-state*) idx))
+                   (setf (aref (orgel-level-sliders orgel) idx) vsl)))
+        (setf (value nb1) (orgel-numbox *curr-orgel-state*))
+        (setf (orgel-numbox orgel) nb1)
+
 ;;;        (setf (width p1) 162)
 ;;;        (setf (height p1) 100)
 ;;;        (setf (background-color p1) "w3-black")
@@ -195,6 +176,9 @@
     (run body)
 ;;;    (remhash connection-id *global-connection-hash*)
     ))
+
+
+
 
 (defun start-orgel-gui ()
   "Start Orgel Gui."
---
+++ widget-defs.lisp
@@ -0,0 +1,151 @@
+;;; 
+;;; widget-defs.lisp
+;;;
+;;; **********************************************************************
+;;; Copyright (c) 2023 Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
+;;;
+;;; Revision history: See git repository.
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the Gnu Public License, version 2 or
+;;; later. See https://www.gnu.org/licenses/gpl-2.0.html for the text
+;;; of this agreement.
+;;; 
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;;; GNU General Public License for more details.
+;;;
+;;; **********************************************************************
+
+(in-package :cl-orgel-gui)
+
+#|
+non-selectable
+user-select (none, bzw. auto)
+
+style: user-select:
+
+UserSelect 
+|#
+
+(defun multi-vslider (container &key (num 8) (width 80) (height 100) (background-color "white") (colors #("lightblue")) (thumbcolor "transparent")
+                                  receiver-fn)
+  (let* ((msl-container (create-div container
+                                    :style (format nil "color: transparent; background-color: transparent;border: none;width: ~Apx;height: ~Apx;display: flex;" width height)))
+         (vsliders (v-collect (n num) (vslider
+                                       msl-container
+                                       :border-right-width (if (< n (1- num)) 0 1)
+                                       :background-color background-color
+                                       :color (aref colors (mod n num))
+                                       :thumbcolor thumbcolor))))
+    (loop for vsl in vsliders
+          for idx from 0
+          do (let ((vsl vsl) (idx idx))
+               (set-on-input
+                vsl
+                (lambda (obj)
+                  (declare (ignore obj))
+                  (let ((val (value vsl)))
+                    (if receiver-fn (funcall receiver-fn idx val vsl)))))
+               (set-on-mouse-move
+                vsl
+                (lambda (obj event-data)
+                  (declare (ignore obj))
+                  (when (getf event-data :shift-key)
+                    (let ((val (- 100 (getf event-data :y))))
+                      (when val
+                        (setf (value vsl) val)
+                        (if receiver-fn (funcall receiver-fn idx val vsl)))))))))
+    (values vsliders msl-container)))
+
+(defun vslider
+    (container &key (value 0.0) (min 0.0) (max 100.0) (thumbcolor "black") (color "#3071A9")
+                 (border-right-width 1) (background-color "#fff")
+                 receiver-fn)
+    "vertical slider including behaviour."
+  (let ((vsl
+          (create-form-element
+           container :range
+           :class "vslider"
+           :style (format nil "--border-right-width: ~Apx;--slider-thumb: ~A;--slider-color: ~A;--slider-background: ~A;min-width: 0;flex: 1 1 0;height: 100%;"
+                          border-right-width thumbcolor color background-color)
+           :value (format nil "~a" value)
+           :min (format nil "~a" min)
+           :max (format nil "~a" max)
+           :orient "vertical")))
+    (if receiver-fn
+        (set-on-input
+         vsl
+         (lambda (obj)
+           (declare (ignore obj))
+           (let ((val (value vsl)))
+;;;                                       (format t "vsl~a: ~a~%" (1+ idx) val)
+             (funcall receiver-fn val vsl)))))
+    vsl))
+
+(defun numbox (container &key (color "#3071A9")
+                           (background-color "#fff")
+                           (selected-foreground "black")
+                           (selected-background "lightblue")
+                           (value 0)
+                           label
+                           label-style)
+  (let ((elem
+          (create-form-element
+           container :text
+           :class "numbox"
+           :value (format nil "~,1f" value)
+           :style (format nil ";--text-color: ~A;background-color: ~A:--numbox-selected-foreground: ~A;--numbox-selected-background: ~A;"
+                          color background-color selected-foreground selected-background)
+           :label (if label (create-label container :content (ensure-string label) :style (or label-style "margin-right: 5px;")))))
+        mouse-dragged
+        startvalue)
+;;;    (clog::unbind-event-script elem "onmousedown")
+    (set-on-mouse-down
+     elem
+     (lambda (obj event-data)
+       (declare (ignore obj))
+       (setf startvalue (read-from-string (or (value elem) "0")))
+       (let ((startpos (getf event-data :y)))
+         (set-on-mouse-move
+          elem
+          (let ((last-y startpos) (last-val startvalue))
+            (lambda (obj event-data)
+              (declare (ignore obj))
+              (let* ((y (getf event-data :y))
+                     (scale (if (getf event-data :shift-key) 0.1 1))
+                     (val (+ last-val (* scale (- last-y y)))))
+                (when (/= y last-y)
+                  (unless mouse-dragged
+                    (setf (style elem "--numbox-selected-foreground") color)
+                    (setf (style elem "--numbox-selected-background") background-color))
+                  (setf mouse-dragged t)
+                  (setf (value elem) (format nil "~,1f" val))
+                  (synchronize-numbox(format nil "~,1f" val) elem)
+                  (setf last-y y last-val val)))))))))
+    (set-on-key-up
+     elem
+     (lambda (obj event)
+       (declare (ignore obj))
+       (when (equal (getf event :key) "Enter")
+         (let ((val (value elem)))
+           (unless (numberp (read-from-string val))
+             (setf val (format nil "~,1f" startvalue)))
+           (setf (value elem) val)
+           (synchronize-numbox val elem))
+         (blur elem))))
+    (set-on-mouse-up
+     elem
+     (lambda (obj event-data)
+       (declare (ignore event-data))
+       (set-on-mouse-move
+        obj
+        (lambda (obj event-data)
+          (declare (ignore obj event-data))))
+       (if mouse-dragged (progn
+                           (blur elem)
+                           (setf (style obj "--numbox-selected-foreground") selected-foreground))
+                           (setf (style obj "--numbox-selected-background") selected-background))
+                           (setf mouse-dragged nil)))
+    elem))
